9c86c0d65937fb1260d8eb8dc4c0e07ad453f46c
livirex <livirex@gmail.com>
2013-11-15 11:34:41 +0600

    Update 404.php
    Убрал багу :)

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ CHTTP::SetStatus("404 Not Found");
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-$APPLICATION->SetTitle("Страница не найдена");
+$APPLICATION->SetTitle("Bug title");
 
 $APPLICATION->IncludeComponent("bitrix:main.map", ".default", array(
 	"CACHE_TYPE" => "A",
